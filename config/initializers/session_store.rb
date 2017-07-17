b0a90f1bc9014bc7917143410eab34c6b1ed3b49
Takayuki Hasegawa <takayuki.hasegawa0311@gmail.com>
2017-07-17 11:49:16 +0900

    Add devise model

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+require 'action_dispatch/middleware/session/dalli_store'
+
+# Share devise session accross over subdomain. https://github.com/plataformatec/devise/wiki/How-To:-Use-subdomains
+Rails.application.config.session_store ActionDispatch::Session::CacheStore,
+                                       cache: ActiveSupport::Cache::MemCacheStore.new(
+                                           ENV['MEMCACHE_SERVERS'].present? ? ENV['MEMCACHE_SERVERS'].split(',') : 'localhost:11211', {
+                                           namespace: 'sessions',
+                                       }),
+                                       expire_after: 2.months,
